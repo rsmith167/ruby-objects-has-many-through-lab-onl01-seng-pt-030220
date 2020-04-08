923d529323e047f4aaf9b17644be72d9b5d677dc
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 20:34:25 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,7 +1,10 @@
 class Appointment
   @@all = []
+  attr_accessor :date, :patient, :doctor
   def initialize(date, patient, doctor)
-    
+    @date = date
+    @patient = patient
+    @doctor = doctor
   end
   def self.all
     @@all
