2335ca1da8f37dde266684a9a5ba4426650f3cb8
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 20:28:14 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,6 +4,13 @@ class Patient
   attr_accessor :name
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def new_appointment(doctor, date)
+    
   end
   
   
