ee9cab5aa85368e53ad0b42019c20551da83890a
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 20:23:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,7 +14,7 @@ class Artist
      Song.new(name, self, genre)
    end
    def songs
-     Songs.all.select {|x| x.artist == self}
+     Song.all.select {|x| x.artist == self}
    end
  def genres
    self.songs.collect {|x| x.genre}
