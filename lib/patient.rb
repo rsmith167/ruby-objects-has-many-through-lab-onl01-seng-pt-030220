5e570a9a3368b7b217b782f7ec8d6b3e6fe235f4
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 20:44:20 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,7 +9,7 @@ class Patient
   def self.all
     @@all
   end
-  def new_appointment(date, doctor)
+  def new_appointment( doctor, date)
     Appointment.new(date, self, doctor)
   end
   def appointments
